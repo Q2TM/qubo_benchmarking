be48fbd8b6513b59ab9510ecd2e408f47d6f02b1
Nathan Kittichaikoonkij <Natsg90@gmail.com>
2025-01-12 17:05:34 +0700

    tsp

--- TSP/benchmark.py
+++ TSP/benchmark.py
@@ -6,40 +6,42 @@ from tqdm import tqdm
 
 from TSP.model import Tsp
 from TSP.core import bruteforce
-from Utils.solvers import GetGurobiClient, GetFixstarsClient, GetDWaveClient
+from Utils.solvers import CreateGurobiClient, CreateFixstarsClient, CreateDWaveClient
 from Utils.measure import run_compare_solvers
 
 
-## Set up clients
-clientG10s = GetGurobiClient(
+# Set up clients
+clientG10s = CreateGurobiClient(
     library_path=r"D:\miniconda3\envs\cwq\gurobi110.dll", timeout_sec=10)
-clientG100s = GetGurobiClient(
+clientG100s = CreateGurobiClient(
     library_path=r"D:\miniconda3\envs\cwq\gurobi110.dll", timeout_sec=100)
-clientFixstars = GetFixstarsClient(timeout=1000)
-clientFixstars10s = GetFixstarsClient(timeout=10_000)
-clientDWave41 = GetDWaveClient("Advantage_system4.1")
-clientDWave64 = GetDWaveClient("Advantage_system6.4")
-clientDWaveV2 = GetDWaveClient("Advantage2_prototype2.6")
+clientFixstars = CreateFixstarsClient(timeout=1000)
+clientFixstars10s = CreateFixstarsClient(timeout=10_000)
+clientDWave41 = CreateDWaveClient("Advantage_system4.1")
+clientDWave64 = CreateDWaveClient("Advantage_system6.4")
+clientDWaveV2 = CreateDWaveClient("Advantage2_prototype2.6")
 
-solvers = [("Gurobi 10s", clientG10s),
-              ("Gurobi 100s", clientG100s),
-    ("Fixstars 1s", clientFixstars),
-    ("Fixstars 10s", clientFixstars10s),]
+solvers = [ ("Gurobi 10s", clientG10s),
+           ("Gurobi 100s", clientG100s),
+           ("Fixstars 1s", clientFixstars),
+           ("Fixstars 10s", clientFixstars10s),
+    ("DWave 4.1", clientDWave41),
+    ("DWave 6.4", clientDWave64),
+   ]
+# Set up problems
+nodes = [4, 5, 6, 7, 8, 10, 12, 15, 20, 42, 69]
+repeat = 7
 
-## Set up problems
-nodes = [4, 5, 6, 7, 8,] # 10, 12, 15, 20, 42, 69]
-repeat = 1
 
-
-def run(nodes: int, solver: Literal['Qiskit', 'Amplify'], penalty: int = 1_000_000)-> tuple[Any, list]:
+def run(nodes: int, solver: Literal['Qiskit', 'Amplify'], penalty: int = 1_000_000) -> tuple[Any, list]:
     """
     Run TSP benchmark for a given node and solver
-    
+
     Args:
         nodes (int): Number of nodes
         solver (str): Solver name
         penalty (int, optional): Penalty. Defaults to 1_000_000.
-    
+
     Returns:
         tuple[CompareResult, list]: Result and errors
     """
@@ -50,34 +52,33 @@ def run(nodes: int, solver: Literal['Qiskit', 'Amplify'], penalty: int = 1_000_0
     create_model_start = time.time()
     qubo = tsp.qubo(penalty=penalty)
     time_model_formulation = time.time() - create_model_start
-    
+
     # bruteforce
     cost, _, time_taken = bruteforce(tsp.matrix, timeout=100)
     bruteforce_result = (cost, time_taken)
-    
+
     # Run with different solvers
-    return run_compare_solvers(nodes, qubo, time_model_formulation, bruteforce=bruteforce_result, amplifys=solvers, 
-                               max_edge_weight=tsp.max_edge_weight,
-                                avg_edge_weight=tsp.avg_edge_weight,
+    return run_compare_solvers(nodes, qubo, time_model_formulation, bruteforce=bruteforce_result, amplifys=solvers,
+                              max_edge_weight=tsp.max_edge_weight,
+                               avg_edge_weight=tsp.avg_edge_weight,
                                 distance_matrix=tsp.matrix,
                                 constraint_weight=penalty,
-                                ) 
+                                )
 
-
-## Run benchmark
+# Run benchmark
 results = []
 for node in tqdm(nodes, desc="Nodes"):
     for it in tqdm(range(repeat), desc="Repeat"):
-        result, errors = run(node, "Amplify", 1_000_000)
+        result, errors = run(node, "Amplify", 15_000_000)
         results.append(result)
 
-        for err in errors:
-            if err.error is not None:
-                print(f"Error at node={node} it={it}")
-                print(err)
+for err in errors:
+    if err.error is not None:
+        print(f"Error at node={node} it={it}")
+        print(err)
 
 # Convert dataclass to dict
-serializable = list(map(lambda x: asdict(x), results))
+serializable= list(map(lambda x: asdict(x), results))
 
 # Convert result into json
 with open("TSP/result.json", "w") as f:
